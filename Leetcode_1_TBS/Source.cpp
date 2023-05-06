47e4331db0065245e7b27ecf4f74dba68074c4ea
Pavel Dashko <dashko.pavel@bk.ru>
2023-05-06 23:46:56 +0300

    Hash table first acquaintance

---
+++ Leetcode_1_TBS/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+class Solution
+{
+public:
+	std::vector<int> twoSum(std::vector<int>& nums, int target)
+	{
+		std::unordered_map<int, int> visited;
+		int len = nums.size();
+		for (int i = 0; i < len; ++i)
+		{
+			int n = nums[i];
+			int complement = target - n;
+			if (visited.count(complement))
+			{
+				return { visited[complement], i
+				};
+			}
+			visited[n] = i;
+		}
+		return {};
+	}
+	
+
+};
+
+int main()
+{
+	return 0;
+}
