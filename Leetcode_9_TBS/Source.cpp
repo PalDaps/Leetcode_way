763541fba21829b46550886db94256132a6b5036
Pavel Dashko <dashko.pavel@bk.ru>
2023-05-07 23:06:19 +0300

    God like

--- Leetcode_9_TBS/Source.cpp
+++ Leetcode_9_TBS/Source.cpp
@@ -3,5 +3,23 @@
 class Solution
 {
 public:
-	bool isPalindrom()
+	bool isPalindrom(int x)
+	{
+		if (x < 0 || x != 0 && x % 10 == 0) return false;
+		int check = 0;
+		while (x > check)
+		{
+			check = check * 10 + x % 10;
+			std::cout << "check = " << check << std::endl;
+			x /= 10;
+			std::cout << "x /= 10 = " << x << std::endl;
+		}
+		return (x == check || x == check / 10);
+	}
 };
+
+int main()
+{
+	Solution daps;
+	daps.isPalindrom(121);
+}
