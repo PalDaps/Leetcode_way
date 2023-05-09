136c7307620e33db1ee4c8b38e773a5a16772ffc
Pavel Dashko <dashko.pavel@bk.ru>
2023-05-10 00:47:33 +0300

    The problem was completed(79%, 65%)

--- Leetcode_13/Source.cpp
+++ Leetcode_13/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 class Solution
 {
@@ -19,6 +20,8 @@ public:
 		int C = 100;
 		int D = 500;
 		int M = 1000;
+		int sum = 0;
+		std::vector<int> numbers;
 		for (int i = 0; i < s.size(); i++)
 		{
 
@@ -26,51 +29,67 @@ public:
 			{ 
 				case roman_I:
 			    {
-					std::cout << I;
+					numbers.push_back(I);
 					break;
 			    }
 			    case roman_V:
 			    {
-					std::cout << V;
+					numbers.push_back(V);
 					break;
 			    }
 			    case roman_X:
 			    {
-					std::cout << X;
+					numbers.push_back(X);
 					break;
 			    }
 			    case roman_L:
 			    {
-					std::cout << L;
+					numbers.push_back(L);
 					break;
 			    }
 			    case roman_C:
 			    {
-					std::cout << C;
+					numbers.push_back(C);
 					break;
 			    }
 				case roman_D:
 				{
-					std::cout << D;
+					numbers.push_back(D);
 					break;
 				}
 				case roman_M:
 				{
-					std::cout << M;
+					numbers.push_back(M);
 					break;
 				}
 			}
-		}
-		return 1;
 
+		}
+		for (int i = 1; i < numbers.size(); i++)
+		{
+			if (numbers[i-1] >= numbers[i])
+			{
+				sum = numbers[i-1] + sum;
+			}
+			else if (numbers[i-1] < numbers[i])
+			{
+				sum = sum + (numbers[i] - numbers[i-1]);
+				i++;
+			}
+		}
+		if (numbers.size() == 1) return numbers[0];
+		if (numbers[numbers.size() - 2] >= numbers[numbers.size() - 1] && numbers.size() != 1)
+			sum = sum + numbers[numbers.size() - 1];
+		return sum;
 	}
 };
 
 int main()
 {
-	std::string roman_number = "III";
+	std::string roman_number = "D";
+	std::string roman_number_two = "MCMXCIV";
 	// std::cout << roman_number[0];
 	Solution daps;
-	daps.romanToInt(roman_number);
+	std::cout << daps.romanToInt(roman_number);
 	return 0;
 }
