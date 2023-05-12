8dc123f5d221a8f48fff4e9b25e5806812a962ae
Pavel Dashko <dashko.pavel@bk.ru>
2023-05-12 22:35:12 +0300

    A very good practice of working with pointers, structures, algorithms, but I got confused at the end. What is being done current_result = list 1 and current_result = current_result->next;

--- Leetcode_21/Source.cpp
+++ Leetcode_21/Source.cpp
@@ -20,61 +20,49 @@ class Solution
 public: 
 	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
 	{
-		ListNode* result;
-		if (list1 == nullptr && list2 == nullptr )
+		ListNode* result = new ListNode(0);
+		ListNode* current_result = result;
+   		while (true)
 		{
-			result = nullptr;
-			return result;
-		}
-		else if (list1 == nullptr)
-		{
-			result = list2;
-			return result;
-		}
-		else if (list2 == nullptr)
-		{
-			result = list1;
-			return result;
-		}
-		else
-		{
-			ListNode* current_one = list1;
-			ListNode* current_two = list2;
-			while (current_one != nullptr)
+			if (list1 == nullptr && list2 == nullptr)
 			{
-				if (current_one->val > current_two->val)
+				return result->next;
+			}
+			else if (list1 == nullptr)
+			{
+				current_result->next = list2;
+				return result->next;
+			}
+			else if (list2 == nullptr)
+			{
+				current_result->next = list1;
+				return result->next;
+			}
+			else
+			{
+				if (list1->val < list2->val)
 				{
-					result = current_two;
-					result->next = current_one;
-					current_one = current_one->next;
-					current_two = current_two->next;
-				}
-				else if (current_one->val < current_two->val)
-				{
-					result = current_one;
-					result->next = current_two;
-					current_one = current_one->next;
-					current_two = current_two->next;
+					current_result->next = list1;
+					current_result = current_result->next;
+					list1 = list1->next;
 				}
 				else
 				{
-					result = current_one;
-					result->next = current_two;
-					current_one = current_one->next;
-					current_two = current_two->next;
+					current_result->next = list2;
+					current_result = current_result->next;
+					list2 = list2->next;
 				}
 			}
-			return result;
 		}
 	}
 };
 
 int main()
 {
-	ListNode* head = new ListNode(5);
+	ListNode* head = new ListNode(1);
 	ListNode* second = new ListNode(2);
 	head->next = second;
-	ListNode* third = new ListNode(3);
+	ListNode* third = new ListNode(4);
 	second->next = third;
 	
 	ListNode* current = head;
@@ -86,10 +74,10 @@ int main()
 	std::cout << std::endl;
 
 
-	ListNode* head_ = new ListNode(8);
-	ListNode* second_ = new ListNode(7);
+	ListNode* head_ = new ListNode(1);
+	ListNode* second_ = new ListNode(3);
 	head_->next = second_;
-	ListNode* third_ = new ListNode(6);
+	ListNode* third_ = new ListNode(4);
 	second_->next = third_;
 
 	current = head_;
@@ -101,17 +89,15 @@ int main()
 	std::cout << std::endl;
 
 	Solution daps;
-	ListNode* result;
-	result = daps.mergeTwoLists(head, head_);
-	current = result;
-	while (result != nullptr)
+	current = daps.mergeTwoLists(head, head_);
+	while (current != nullptr)
 	{
-		std::cout << result->val << " ";
-		result = result->next;
+		std::cout << current->val << " ";
+		current = current->next;
 	}
 
 	// delete memory
-	current = head;
+	/* current = head;
 	while (current != nullptr)
 	{
 		ListNode* temp = current;
@@ -134,6 +120,5 @@ int main()
 		delete temp;
 	}
 	*/
-
 	return 0;
 }
