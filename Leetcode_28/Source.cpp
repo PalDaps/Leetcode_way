734df43f5a8cf9bf4ee659dbe5b04e8debee561c
Pavel Dashko <dashko.pavel@bk.ru>
2023-05-13 22:51:27 +0300

    Done!

--- Leetcode_28/Source.cpp
+++ Leetcode_28/Source.cpp
@@ -6,19 +6,21 @@ public:
 	int strStr(std::string haystack, std::string needle)
 	{
 		int counter = 0;
-		for (int i = 1; i < haystack.length(); i++)
+		if (haystack == needle) return 0;
+		for (int i = 0; i < haystack.length(); i++)
 		{
-			if (haystack[i - 1] == needle[0] && haystack[i] == needle[1])
+			if ( haystack[i] == needle[0] )
 			{
-				int k = 2;
-				counter = 2;
-				for (int j = i+1; j < needle.length(); j++)
+				int k = i+1;
+				counter = 1;
+				if (needle.length() == 1) return i;
+				for (int j = 1; j < needle.length(); j++)
 				{
-					if (haystack[j] == needle[k])
+					if (haystack[k] == needle[j])
 					{
 						counter++;
 						k++;
-						if (counter == needle.length()) return i - 1;
+						if (counter == needle.length()) return i;
 					}
 				}
 			}
@@ -36,6 +38,8 @@ int main()
 	std::string d = "leeto";
 	std::string x = "hello";
 	std::string y = "ll";
-	std::cout << daps.strStr(c, d);
+	std::string v = "abc";
+	std::string n = "c";
+	std::cout << daps.strStr(v, n);
 	return 0;
 }
