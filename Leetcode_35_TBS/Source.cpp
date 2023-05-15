61ded09b7943d0255fb2ae04aa7ad9f92011157f
Pavel Dashko <dashko.pavel@bk.ru>
2023-05-15 23:04:59 +0300

    An interesting solution. It adapts binary search to the condition of the problem. To get this solution, you first need to analyze vectors of size 2, then 3, and so you can isolate this solution.

---
+++ Leetcode_35_TBS/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+	int searchInsert(std::vector<int>& nums, int target)
+	{
+		int l = 0;
+		int r = nums.size() - 1;
+		while (l < r)
+		{
+			int middle = (l + (r - l)) / 2; // One of the most difficult lines to understand in this code. It is needed to avoid overflow.
+			if (nums[middle] == target)
+				return middle;
+			else if (target > nums[middle])
+				l = middle + 1;
+			else
+				r = middle;
+		}
+		return nums[l] < target ? l + 1 : l; // Also an interesting line, which is difficult to understand. In general, it checks the last element that has not been checked by the main algorithm.
+	}
+};
+
+int main()
+{
+	return 0;
+}
