e268fde81d25852fb578ffd4763a2b2d69ffecec
Pavel Dashko <dashko.pavel@bk.ru>
2023-05-15 22:33:18 +0300

    A good result ( Runtime 3 ms Beats 88.75%) and ( Memory 9.6 MB Beats 74.92%), but reached the solution on crutches. I am sure there is an ideal solution.

--- Leetcode_35/Source.cpp
+++ Leetcode_35/Source.cpp
@@ -6,10 +6,17 @@ class Solution
 public:
 	int searchInsert(std::vector<int>& nums, int target)
 	{
+		if (nums.size() == 1)
+		{
+			if (nums[0] < target)
+				return 1;
+			else
+				return 0;
+		}
 		int l = 0;
 		int r = nums.size();
 		int middle = (l + r) / 2;
-		while (l < r)
+		while (l <= r )
 		{
 			if (target == nums[middle])
 			{
@@ -24,21 +31,27 @@ public:
 				r = middle - 1;
 			}
 			middle = (l + r) / 2;
+			if (middle > nums.size() - 1)
+				break;
 		}
 		for (int i = 0; i < nums.size()-1; i++)
 		{
 			if (nums[i] < target && target < nums[i + 1])
-				return i+1;
+				return i + 1;
+			else if (nums[nums.size() - 1] < target)
+				return nums.size();
 		}
-		return nums.size();
+		return 0;
 	}
 };
 
 
 int main()
 {
-	std::vector<int> anime = { 1, 3, 5, 6 };
+	std::vector<int> anime = { 1,4,6,7,8,9 };
+	std::vector<int> test2 = { 1 };
+	std::vector<int> test3 = { 1, 3, 5, 6 };
 	Solution daps;
-	std::cout << daps.searchInsert(anime, 4);
+	std::cout << daps.searchInsert(test3, 5);
 	return 0;
 }
