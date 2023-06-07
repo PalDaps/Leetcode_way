087708df0b809a3171888c5741168b84e28dafcd
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-07 16:10:29 +0300

    One more version of this hard problem

--- Leetcode_1071/Source.cpp
+++ Leetcode_1071/Source.cpp
@@ -5,11 +5,13 @@ class Solution {
 public:
 	std::string gcdOfString(std::string str1, std::string str2) {
 		std::string substring = "";
+		std::string result = "";
+		std::string result_two = "";
 		bool flag = 0;
 		for (int i = 1; i < str1.size(); i++) {
 			std::string temp = "";
 			temp = str1.substr(0, i);
-			for (int j = 0; j < str1.size()-i+1 || substring.size()>str1.size(); j++) {
+			for (int j = 0; j < str1.size()-i+1 || substring.size()<str1.size(); j++) {
 				for (int k = 0; k < temp.size(); k++) {
 				substring.push_back(temp[k]);
 				}
@@ -23,13 +25,24 @@ public:
 			if (flag) break;
 			substring = "";
 		}
-		return "";
+		for (int i = 0; i < std::min(substring.size(), str2.size()); i++) {
+			if (substring[i] == str2[i])
+				result.push_back(substring[i]);
+			else
+				break;
+		}
+		for (int i = 0; result_two.size() < str1.size(); i++) {
+			result_two = result_two + str2;
+			if (result_two == str1)
+				return str2;
+		}
+		return result;
 	}
 };
 
 int main() {
-	std::string test = "ABCABCABCABCABCABC";
-	std::string test1 = "ABAB";
+	std::string test = "AAAAAAAAA";
+	std::string test1 = "AAACCC";
 	Solution daps;
 	std::cout << daps.gcdOfString(test, test1);
 	return 0;
