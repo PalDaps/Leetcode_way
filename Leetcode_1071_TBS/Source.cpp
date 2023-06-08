8ed79bdf0333c16496b41ecc82a1523885753b01
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-08 20:04:07 +0300

    Repeat this. My memmory is sheet

--- Leetcode_1071_TBS/Source.cpp
+++ Leetcode_1071_TBS/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+/*
 class Solution {
 public:
 	std::string gcdOfStrings(std::string str1, std::string str2) {
@@ -15,5 +16,25 @@ public:
 		}
 		return (str1 + str2 == str2 + str1) ? str1.substr(0, b) : "";
 	}
+
 	
+};
+*/
+
+// after 30 min
+
+class Solution {
+public:
+	std::string gcdOfStrings(std::string str1, std::string str2) {
+		int ost = 1;
+		int a = str1.size();
+		int b = str2.size();
+		if (a > b) std::swap(a, b);
+		while (ost != 0) {
+			ost = b % a;
+			b = a;
+			a = ost;
+		}
+		return (str1 + str2 == str2 + str2) ? str1.substr(0, b) : "";
+	}
 };
