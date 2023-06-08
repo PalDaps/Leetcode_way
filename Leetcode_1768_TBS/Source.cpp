f9b72ee14701136fd09973f60dc2a156d2b7d4c3
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-08 19:11:46 +0300

    I try to remember

--- Leetcode_1768_TBS/Source.cpp
+++ Leetcode_1768_TBS/Source.cpp
@@ -36,6 +36,7 @@ public:
 };
 */
 // after a day
+/*
 class Solution {
 public:
 	std::string mergeAlternately(std::string word1, std::string word2) {
@@ -52,7 +53,22 @@ public:
 		return result;
 	}
 };
-
+*/
+// after 2 days
+class Solution {
+public: 
+	std::string mergeAlternately(std::string word1, std::string word2) {
+		std::string result = "";
+		int n = word1.size(), m = word2.size(), i = 0, j = 0;
+		while (i < n || j < m) {
+			if (i < n)
+				result.push_back(word1[i++]);
+			if (j < m)
+				result.push_back(word2[j++]);
+		}
+		return result;
+	}
+};
 // after 5 days i need check
 
 int main() {
