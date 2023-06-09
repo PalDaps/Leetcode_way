adda38ce32c8b69d8b4f6692e870bb7fb73378e2
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-09 15:54:06 +0300

    repeated 1071

--- Leetcode_1071_TBS/Source.cpp
+++ Leetcode_1071_TBS/Source.cpp
@@ -22,7 +22,7 @@ public:
 */
 
 // after 30 min
-
+/*
 class Solution {
 public:
 	std::string gcdOfStrings(std::string str1, std::string str2) {
@@ -36,5 +36,26 @@ public:
 			a = ost;
 		}
 		return (str1 + str2 == str2 + str2) ? str1.substr(0, b) : "";
+	}
+};
+*/
+
+// after a day 6/9/2023
+// succes
+class Solution {
+public:
+	std::string gcdOfStrings(std::string str1, std::string str2) {
+		int ost = 1;
+		int a = str1.size();
+		int b = str2.size();
+		if (a > b) {
+			std::swap(a, b);
+		}
+		while (ost != 0) {
+			ost = b % a;
+			b = a;
+			a = ost;
+		}
+		return str1 + str2 == str2 + str1 ? str1.substr(0, b) : "";
 	}
 };
