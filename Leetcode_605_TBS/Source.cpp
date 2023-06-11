6b17bbc4d7fec4870c5110247626d45ca8d5dac7
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-11 22:00:48 +0300

    Remember after 10 min the 605_TBS

--- Leetcode_605_TBS/Source.cpp
+++ Leetcode_605_TBS/Source.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include <vector>
 
+/*
 class Solution {
 public:
 	bool canPlaceFlowers(std::vector<int>& flowerbed, int n) {
 		flowerbed.insert(flowerbed.begin(), 0);
 		flowerbed.push_back(0);
 		for (int i = 1; i < flowerbed.size() - 1; i++) {
+			if (flowerbed[i - 1] + flowerbed[i] + flowerbed[i + 1] == 0) {
+				n--;
+				i++;
+			}
+		}
+		return n <= 0;
+	}
+};
+*/
+
+//after 10 min checked 6/11/2023
+
+class Solution {
+public:
+	bool canPlaceFlowers(std::vector<int>& flowerbed, int n) {
+		flowerbed.insert(flowerbed.begin(), 0);
+		flowerbed.push_back(0);
+		for (int i = 1; i < flowerbed.size()-1; i++) {
 			if (flowerbed[i - 1] + flowerbed[i] + flowerbed[i + 1] == 0) {
 				n--;
 				i++;
