14957665cf8a5667a2ac7a2c3c429a69f5923822
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-13 19:13:09 +0300

    Not a bad option, but I'm sure he won't pass all the test

--- Leetcode_345/Source.cpp
+++ Leetcode_345/Source.cpp
@@ -3,108 +3,110 @@
 class Solution {
 public:
 	std::string reverseVowels(std::string s) {
-		int prev = 1, now = 0;
-		for (int i = 0; i < s.size(); i++) {
-			switch (s[i]) {
-			case 'a':
-				prev = i;
-				now = i;
-				break;
-			case 'A':
-				prev = i;
-				now = i;
-				break;
-			case 'e':
-				prev = i;
-				now = i;
-				break;
-			case 'E':
-				prev = i;
-				now = i;
-				break;
-			case 'i':
-				prev = i;
-				now = i;
-				break;
-			case 'I':
-				prev = i;
-				now = i;
-				break;
-			case 'o':
-				prev = i;
-				now = i;
-				break;
-			case 'O':
-				prev = i;
-				now = i;
-				break;
-			case 'u':
-				prev = i;
-				now = i;
-				break;
-			case 'U':
-				prev = i;
-				now = i;
-				break;
-			default:
-				break;
-			}
-			if (prev == now) break;
-		}
-		for (int i = 0; i < s.size(); i++) {
-			switch (s[i]) {
+		int safe_i = 0, safe_j = 0;
+		bool flag_i = false;
+		bool flag_j = false;
+		for (int i = 0, j = s.size()-1; i < j; i++, j--) {
+			if (!flag_j) {
+				switch (s[i]) {
 				case 'a':
-					prev = i;
-					if (now != prev) std::swap(s[prev], s[now]);
-					now = i;
+					safe_i = i;
+					flag_i = true;
 					break;
 				case 'A':
-					prev = i;
-					if (now != prev) std::swap(s[prev], s[now]);
-					now = i;
+					safe_i = i;
+					flag_i = true;
 					break;
 				case 'e':
-					prev = i;
-					if (now != prev) std::swap(s[prev], s[now]);
-					now = i;
+					safe_i = i;
+					flag_i = true;
 					break;
 				case 'E':
-					prev = i;
-					if (now != prev) std::swap(s[prev], s[now]);
-					now = i;
+					safe_i = i;
+					flag_i = true;
 					break;
 				case 'i':
-					prev = i;
-					if (now != prev) std::swap(s[prev], s[now]);
-					now = i;
+					safe_i = i;
+					flag_i = true;
 					break;
 				case 'I':
-					prev = i;
-					if (now != prev) std::swap(s[prev], s[now]);
-					now = i;
+					safe_i = i;
+					flag_i = true;
 					break;
 				case 'o':
-					prev = i;
-					if (now != prev) std::swap(s[prev], s[now]);
-					now = i;
+					safe_i = i;
+					flag_i = true;
 					break;
 				case 'O':
-					prev = i;
-					if (now != prev) std::swap(s[prev], s[now]);
-					now = i;
+					safe_i = i;
+					flag_i = true;
 					break;
 				case 'u':
-					prev = i;
-					if (now != prev) std::swap(s[prev], s[now]);
-					now = i;
+					safe_i = i;
+					flag_i = true;
 					break;
 				case 'U':
-					prev = i;
-					if (now != prev) std::swap(s[prev], s[now]);
-					now = i;
+					safe_i = i;
+					flag_i = true;
 					break;
 				default:
+					flag_i = false;
 					break;
+				}
+			}
+			if (flag_i) {
+				switch (s[j]) {
+				case 'a':
+					safe_j = j;
+					flag_j = true;
+					break;
+				case 'A':
+					safe_j = j;
+					flag_j = true;
+					break;
+				case 'e':
+					safe_j = j;
+					flag_j = true;
+					break;
+				case 'E':
+					safe_j = j;
+					flag_j = true;
+					break;
+				case 'i':
+					safe_j = j;
+					flag_j = true;
+					break;
+				case 'I':
+					safe_j = j;
+					flag_j = true;
+					break;;
+				case 'o':
+					safe_j = j;
+					flag_j = true;
+					break;
+				case 'O':
+					safe_j = j;
+					flag_j = true;
+					break;
+				case 'u':
+					safe_j = j;
+					flag_j = true;
+					break;
+				case 'U':
+					safe_j = j;
+					flag_j = true;
+					break;
+				default:
+					flag_j = false;
+					break;
+				}
+			}
+			if (!flag_i && !flag_j) j++;
+			if (flag_i && !flag_j) i--;
+			if (flag_i && flag_j) {
+				std::swap(s[safe_i], s[safe_j]);
+				flag_j = false;
+				flag_i = false;
 			}
 		}
 		return s;
@@ -113,7 +115,7 @@ public:
 
 int main() {
 	Solution daps;
-	std::string test = "Aa";
+	std::string test = "hello";
 	std::cout << daps.reverseVowels(test);
 	return 0;
 }
