a460b8d662514c67a717bb28fac8cd07b863b3ed
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-15 19:43:31 +0300

    Not a bad version

--- Leetcode_151/Source.cpp
+++ Leetcode_151/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 class Solution {
 public:
@@ -7,44 +8,74 @@ public:
 		std::string result = "";
 		int EndWord = size - 1;
 		bool flag = false;
-		/*for (int i = s.size() - 1; i >= 0; i--) {
-			if (s[i] != ' ' && s[i + 1] == ' ') {
-				EndWord = i;
+		int counter_of_void = 0;
+		int counter_of_symb = 0;
+		for (int i = s.size() - 1; i >= 0; i--) {
+			if (s[i] == ' ') {
+				counter_of_void++;
+				counter_of_symb = 0;
 			}
+			if (s[i] != ' ') {
+				counter_of_symb++;
+				EndWord = counter_of_symb;
+			}
+			/*if (s[i] != ' ' && s[i + 1] == ' ') {
+				EndWord = i;
+			}*/
 			if ((s[i] == ' ' || i == 0) && s[i + 1] != ' ') {
-				if ( i == 0) {
-					result += s.substr(0, EndWord+1);
+				if ( i == 0 && s[i] != ' ') {
+					std::string temp = s.substr(0, EndWord);
+					if (temp != "") result += temp;
 					std::cout << s.substr(0, EndWord+1) << std::endl;
 				}
 				else {
-					result += s.substr(i + 1, EndWord);
+					std::string temp = s.substr(i + 1, EndWord);
+					if (temp == "") EndWord--;
+					if (temp != "") {
+						result += temp;
+						if (s[i-1] == ' ') result.push_back(' ');
+					}
 					std::cout << s.substr(i + 1, EndWord) << std::endl;
-					result.push_back(' ');
+					
 				}
 			}
-		}*/
-		for (int i = size - 1; i >= 0; i--) {
-			if (s[i] == ' ' && !flag) {
-				// std::cout << i << std::endl;
-				flag = true;
+		}
+		if (result[result.size() - 1] == ' ')
+			result.pop_back();
+		//for (int i = size - 1; i >= 0; i--) {
+		//	if (s[i] == ' ' && !flag) {
+		//		// std::cout << i << std::endl;
+		//		flag = true;
+		//	}
+		//	if (s[i] != ' ' && flag) {
+		//		std::cout << i << std::endl;
+		//		flag = false;
+		//	}
+		//}
+		/*std::vector<int> ixes;
+		for (int i = 0; i < size; i++) {
+			if (s[i] != ' ') {
+				ixes.push_back(i);
 			}
-			if (s[i] != ' ' && flag) {
-				std::cout << i << std::endl;
-				flag = false;
+			else {
 			}
 		}
+		for (auto i : ixes)
+			std::cout << i << " ";
+		std::cout << std::endl;*/
 		return result;
 	}
 
 };
 
 int main() {
-	std::string test1 = " hello  world ";
+	std::string test1 = "      ";
 	std::string test2 = "A B";
 	std::string res = "";
 	std::string dob = test1.substr(6, test1.size()-1);
 	res += dob;
-	// std::cout << res;
+	// std::cout << res << std::endl;
+	// std::cout << res.size() << std::endl;
 	Solution daps;
 	std::cout << daps.reverseWords(test1);
 	return 0;
