a735e9d189cf2210338d199786b79df36b423ed2
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-16 17:27:39 +0300

    step by step

--- Leetcode_151_TBS/Source.cpp
+++ Leetcode_151_TBS/Source.cpp
@@ -1,13 +1,68 @@
 #include <iostream>
+#include <vector>
 
+
+// The solution is different from mine, but it is much more compact and faster than mine.
+//class Solution {
+//public:
+//    std::string reverseWords(std::string s) {
+//        reverse(s.begin(), s.end());
+//        int l = 0, r = 0, i = 0, n = s.size();
+//        while (i < n) {
+//            while (i < n && s[i] != ' ')
+//                s[r++] = s[i++];
+//
+//            if (l < r) { // if we can find a non-empty word then
+//                reverse(s.begin() + l, s.begin() + r); // reverse current word
+//                if (r == n) break;
+//                s[r++] = ' '; // set empty space
+//                l = r;
+//            }
+//            ++i; // now i == n or s[i] == ' ', so we skip that character!
+//        }
+//        if (r > 0 && s[r - 1] == ' ') --r; // skip last empty character if have
+//        s.resize(r);
+//        return s;
+//    }
+//};
+
+// after 2 min
+// I want to try to reproduce this solution after I went 
+// through the debugger and memorized some key points and code.
+//
+
+// I hope I remembered something.
 class Solution {
 public:
-	std::string reverseWords(std::string s) {
-
-	}
+    std::string reverseWords(std::string s) {
+        std::reverse(s.begin(), s.end());
+        int i = 0; // обычный счетчик usual counter
+        int n = s.size();
+        int r = 0; // еще один счетчик для того, чтобы компенсировать пробелы
+        int l = 0; // начальная позиция слова
+        while (i < n) {
+            while (i < n && s[i] != ' ') {
+                s[r++] = s[i++];
+            }
+            if (l < r) {
+                reverse(s.begin() + l, s.begin() + r);
+                if (r == n) break;
+                s[r++] = ' ';
+                l = r;
+            }
+            i++;
+        }
+        if (r > 0 && s[r - 1] == ' ') r--;
+        s.resize(r);
+        return s;
+    }
 };
 
+
 int main() {
-	std::cout << "HELLO!";
+    std::string test1 = " hello world ";
+    Solution daps;
+    daps.reverseWords(test1);
+	// std::cout << "HELLO!";
 	return 0;
 }
--- Leetcode_345_TBS/Source.cpp
+++ Leetcode_345_TBS/Source.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 
+//class Solution {
+//public:
+//	std::string reverseVowels(std::string s) {
+//		int i = 0, j = s.size() - 1;
+//		while (i < j) {
+//			i = s.find_first_of("aeiouAEIOU", i);
+//			j = s.find_last_of("aeiouAEIOU", j);
+//			if (i < j) std::swap(s[i++], s[j--]);
+//		}
+//		return s;
+//	}
+//};
+
+
+// after 20 min 6/16/2023
+// succes
 class Solution {
 public:
-	std::string reverseVowels(std::string s) {
-		int i = 0, j = s.size() - 1;
+	std::string reversVowels(std::string s) {
+		/*int i = 0, j = s.size() - 1;
 		while (i < j) {
 			i = s.find_first_of("aeiouAEIOU", i);
 			j = s.find_last_of("aeiouAEIOU", j);
 			if (i < j) std::swap(s[i++], s[j--]);
 		}
+		return s;*/
+		int i = 0, j = s.size() - 1;
+		while (i < j) {
+			while (i < j && !isVowel(s[i])) i++;
+			while (i < j && !isVowel(s[j])) j--;
+			if (i < j) std::swap(s[i++], s[j--]);
+		}
 		return s;
+	}
+private:
+	bool isVowel(char c) {
+		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' ||
+			c == 'E' || c == 'I' || c == 'O' || c == 'U';
 	}
 };
