0406aca543b1d83bcd2e0875aa7b20205b133816
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-22 00:12:24 +0300

    I don't think the speed of this algorithm is O(N)... But this solution, according to leetcode, is correct, even not bad in terms of speed and memory

--- Leetcode_238/Source.cpp
+++ Leetcode_238/Source.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <numeric>
 
 class Solution {
 public:
 	std::vector<int> productExceptSelf(std::vector<int>& nums) {
-		for (int i = 1; i < nums.size(); i++) {
-
+		int counter = 0, composition = 1;
+		for (int i = 0; i < nums.size(); i++) {
+			if (nums[i] != 0) composition = nums[i] * composition;
+			else counter++;
 		}
-
+		for (int i = 0; i < nums.size(); i++) {
+			if (counter == 0) {
+				nums[i] = composition * pow(nums[i], -1);
+			}
+			else if (counter == 1) {
+				if (nums[i] == 0) {
+					nums[i] = 1;
+					nums[i] = composition * pow(nums[i], -1);
+				}
+				else {
+					nums[i] = (counter-1) * pow(nums[i], -1);
+				}
+			}
+			else nums[i] = 0;
+		}
+		for (auto i : nums)
+			std::cout << i << " ";
+		return nums;
 	}
 };
 
 int main() {
-
+	Solution daps;
+	std::vector<int> test = { -1,1,0,-3,3 };
+	daps.productExceptSelf(test);
 	return 0;
 }
