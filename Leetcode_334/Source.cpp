84d96c6352e2b9a7aac82acbbd49f9526b283959
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-24 21:28:23 +0300

    solution with std::sort is even slower than mine

--- Leetcode_334/Source.cpp
+++ Leetcode_334/Source.cpp
@@ -18,7 +18,7 @@
 class Solution {
 public:
 	bool increasingTriplet(std::vector<int>& nums) {
-		for (int i = 1; i < nums.size(); i++) {
+		/*for (int i = 1; i < nums.size(); i++) {
 			bool first = false;
 			bool third = false;
 			for (int pre = 0; pre < i; pre++) {
@@ -34,6 +34,13 @@ public:
 				}
 			}
 			if (first && third) return true;
+		}*/
+		for (int i = 1; i < nums.size(); i++) {
+			std::vector<int> temp = nums;
+			std::sort(temp.begin(), temp.begin() + i);
+			std::sort(temp.begin() + i + 1, temp.end());
+			if (temp[0] < temp[i] && temp[i] < temp[nums.size()-1])
+				return true;
 		}
 		return false;
 	}
@@ -41,7 +48,14 @@ public:
 
 int main() {
 	Solution daps;
-	std::vector<int> test = { 5,1,6 };
+	std::vector<int> test = { 20,100,10,12,5,13 };
+	std::cout << "NOT SORT" << std::endl;
+	for (auto i : test)
+		std::cout << i << " ";
+	std::cout << std::endl << "SORTED" << std::endl;
+	std::sort(test.begin(), test.begin()+3);
+	for (auto i : test)
+		std::cout << i << " ";
 	std::cout << daps.increasingTriplet(test);
 	return 0;
 }
