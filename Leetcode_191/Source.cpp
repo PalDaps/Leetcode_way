2a15a41d669191d913d4a7978372f7f9321d0cfe
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-25 17:20:34 +0300

    Its true version!

---
+++ Leetcode_191/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Solution {
+public:
+	int hammingWeight(uint32_t n) {
+		int counter = 0;
+		for (int i = 0; i < 32; i++) {
+			uint32_t temp = n;
+			temp = temp >> i;
+			counter = counter + (temp & 1);
+		}
+		return counter;
+	}
+};
+
+int main() {
+	uint32_t n = 0b1100;
+	// std::cout << n << std::endl;
+	uint32_t b = 0001;
+	uint32_t res = n & b;
+	// std::cout << res << std::endl;
+	// n = n >> 1; // ×ÒÎ áëèí?
+	uint32_t c = 1;
+	// n = n >> c;
+	// std::cout << n;
+	Solution daps;
+	std::cout << daps.hammingWeight(n);
+}
