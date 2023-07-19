30734cc6c9aa4db2f6844b137f26796f892e69bf
Pavel Dashko <dashko.pavel@bk.ru>
2023-07-19 16:12:38 +0300

    A good solution to the water container problem. I don't know why, but I just remembered it and I don't want to figure out why it is exactly like that.

---
+++ Leetcode_11_TBS/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+
+// 4 : 09 PM
+// 7 / 19 / 2023
+class Solution {
+public:
+	int maxArea(std::vector<int>& height) {
+		int sq = 0;
+		int l = 0;
+		int r = height.size() - 1;
+		while (l < r) {
+			sq = std::max(sq, (r - l) * std::min(height[l], height[r]));
+			if (height[l] < height[r]) {
+				l++;
+			}
+			else
+				r--;
+		}
+		return sq;
+	}
+};
