a3d6f4be4bf147ae02399a60952f9044386df731
Pavel Dashko <dashko.pavel@bk.ru>
2023-07-22 17:46:21 +0300

    This is TBS solution, but not my

--- Leetcode_1679/Source.cpp
+++ Leetcode_1679/Source.cpp
@@ -33,22 +33,62 @@
 // 4 : 50 PM
 // The version without recursion, but i still have TLE
 
+// 5 : 07 PM
+// Okay, I realized something, the campaign does not need to 
+// remove elements from the original array
+
+//class Solution {
+//public:
+//	int maxOperations(std::vector<int>& nums, int k) {
+//		std::unordered_map<int, int> map;
+//		std::vector<int> temp;
+//		int c = 0;
+//		for (int i = 0; i < nums.size(); i++) {
+//			int delta = k - nums[i];
+//			if (map.count(delta)) {
+//				c++;
+//				nums.erase(nums.begin() + i);
+//				nums.erase(nums.begin() + map[delta]);
+//				i = -1;
+//				map.clear();
+//			}
+//			if ( i >=0 ) map[nums[i]] = i;
+//		}
+//		return c;
+//	}
+//};
+
+// 5 : 33 PM
+// Not working solution
+
+//class Solution {
+//public:
+//	int maxOperations(std::vector<int>& nums, int k) {
+//		std::unordered_map<int, int> map;
+//		int c = 0;
+//		for (int i = 0; i < nums.size(); i++) {
+//			int delta = k - nums[i];
+//			if (map.count(delta)) {
+//				c++;
+//				map.erase(delta);
+//			}
+//			else map[nums[i]] = i;
+//		}
+//		return c;
+//	}
+//};
+
 class Solution {
 public:
 	int maxOperations(std::vector<int>& nums, int k) {
 		std::unordered_map<int, int> map;
-		std::vector<int> temp;
 		int c = 0;
-		for (int i = 0; i < nums.size(); i++) {
-			int delta = k - nums[i];
-			if (map.count(delta)) {
+		for (auto i : nums) {
+			if (map[k - i] > 0) {
 				c++;
-				nums.erase(nums.begin() + i);
-				nums.erase(nums.begin() + map[delta]);
-				i = -1;
-				map.clear();
+				map[k - i]--;
 			}
-			if ( i >=0 ) map[nums[i]] = i;
+			else map[i]++;
 		}
 		return c;
 	}
@@ -58,6 +98,6 @@ int main() {
 	std::vector<int> test1 = { 1,2,3,4 };
 	std::vector<int> test2 = { 2, 2, 2, 3, 1, 1, 4, 1 };
 	Solution daps;
-	daps.maxOperations(test2, 4);
+	daps.maxOperations(test1, 5);
 	return 0;
 }
