d7a601d5b813d7918074de9db440c866aea679ba
Pavel Dashko <dashko.pavel@bk.ru>
2023-07-22 16:51:50 +0300

    The version without recursion, but still TLE

--- Leetcode_1679/Source.cpp
+++ Leetcode_1679/Source.cpp
@@ -7,12 +7,37 @@
 // 7 / 22 / 2023
 
 // 4 : 15 There was an idea with recursion
-// 4 : 38 This 
+// 4 : 38 This a working version of problem with recursion 
+// and hash table problem, but time limit exceeded(TLE)
+
+//class Solution {
+//public:
+//	int maxOperations(std::vector<int>& nums, int k) {
+//		std::unordered_map<int, int> map;
+//		int c = 0;
+//		for (int i = 0; i < nums.size(); i++) {
+//			int delta = k - nums[i];
+//			if (map.count(delta)) {
+//				c++;
+//				nums.erase(nums.begin() + i);
+//				nums.erase(nums.begin() + map[delta]);
+//				if (!nums.empty())
+//					c += maxOperations(nums, k);
+//			}
+//			if (!nums.empty()) map[nums[i]] = i;
+//		}
+//		return c;
+//	}
+//};
+
+// 4 : 50 PM
+// The version without recursion, but i still have TLE
 
 class Solution {
 public:
 	int maxOperations(std::vector<int>& nums, int k) {
 		std::unordered_map<int, int> map;
+		std::vector<int> temp;
 		int c = 0;
 		for (int i = 0; i < nums.size(); i++) {
 			int delta = k - nums[i];
@@ -20,10 +45,10 @@ public:
 				c++;
 				nums.erase(nums.begin() + i);
 				nums.erase(nums.begin() + map[delta]);
-				if (!nums.empty())
-					c += maxOperations(nums, k);
+				i = -1;
+				map.clear();
 			}
-			if (!nums.empty()) map[nums[i]] = i;
+			if ( i >=0 ) map[nums[i]] = i;
 		}
 		return c;
 	}
