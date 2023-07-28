e4278c2f8f47a11e14881c47c0721c9bcf92612f
Pavel Dashko <dashko.pavel@bk.ru>
2023-07-28 16:43:42 +0300

    One of the most difficult and beautiful solutions I've seen

---
+++ Leetcode_1004_TBS/Source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+// The first collision with TBS
+// 4 : 21 PM
+// 7 / 28 / 2023
+
+//class Solution {
+//public:
+//	int longestOnes(std::vector<int>& nums, int k) {
+//		int i = 0, j = 0;
+//		for (j = 0; j < nums.size(); j++) {
+//			if (nums[j] == 0) k--;
+//			if (k < 0 && nums[i++] == 0) k++;
+//		}
+//		return j - i;
+//	}
+//};
+
+// 4 : 40 PM / after a 30 minute
+// 7 / 28 / 2023 / 50-50%
+
+class Solution {
+public:
+	int longestOnes(std::vector<int>& nums, int k) {
+		int i = 0, j = 0;
+		for (j = 0; j < nums.size(); j++) {
+			if (nums[j] == 0) k--;
+			if (k < 0 && nums[i++] == 0) k++;
+		}
+		return j - i;
+	}
+};
+
+int main() {
+	std::cout << "HELLO!" << std::endl;
+	std::vector<int> test = { 1,1,1,0,0,0,1,1,1,1,0 };
+	Solution daps;
+	daps.longestOnes(test, 2);
+
+	return 0;
+}
