793d89321eb2ecf08e37adb2c4632d9911a66a23
Pavel Dashko <dashko.pavel@bk.ru>
2023-08-12 19:31:42 +0300

    Remember the 1493 problem

--- Leetcode_1493_TBS/Source.cpp
+++ Leetcode_1493_TBS/Source.cpp
@@ -1,1 +1,26 @@
 #include <iostream>
+#include <vector>
+
+class Solution {
+public:
+	int longestSubarray(std::vector<int>& nums) {
+		int cZeroes = 0;
+		int lWindow = 0;
+		for (int i = 0, begin = 0; i < nums.size(); i++) {
+			cZeroes += (nums[i] == 0);
+			while (cZeroes > 1) {
+				cZeroes -= (nums[begin] == 0);
+				begin++;
+			}
+			lWindow = std::max(lWindow, i - begin);
+		}
+		return lWindow;
+	}
+};
+
+// 6 : 20 PM
+// 8 / 12 / 2023 / SUCCESS
+
+// 7 : 30 PM
+// 8 / 12 / 2023 / SUCCESS
+// After an hour
