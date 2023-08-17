44b6ca7550033c5a0ca35bf6c3d80286a084786b
Pavel Dashko <dashko.pavel@bk.ru>
2023-08-17 18:25:55 +0300

    I have no idea how else it can be accelerated

---
+++ Leetcode_1732_TBS/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+// 6 : 25 PM
+// 8 / 17 / 2023
+// I have no idea how else it can be accelerated
+class Solution {
+	int largestAltitud(std::vector<int>& gain) {
+		int max = 0;
+		for (int i = 0, temp = 0; i < gain.size(); i++) {
+			temp += gain[i];
+			if (temp > max) max = temp;
+		}
+		return max;
+	}
+};
+
+int main() {
+	std::cout << "Google" << std::endl;
+	return 0;
+}
