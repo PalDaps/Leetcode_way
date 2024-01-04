cac29763d89a04d43105e05c44b4b39ca9b2ed48
Daps <dashko.pavel@bk.ru>
2024-01-04 19:45:36 +0300

    The most difficult easy task.  I did it!

--- Leetcode_821/Source.cpp
+++ Leetcode_821/Source.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
 #include <vector>
 
+
 class Solution
+{
+public:
+    std::vector<int> shortestToChar(std::string String, char Char)
+    {
+        std::vector<int> Result(String.size());
+        for (int i = 0; i < String.size(); i++)
+        {
+            if (String[i] == 'Char') Result[i] = 0;
+            else
+            {
+                int LeftDist = GoToLeft(String, Char, i);
+                int RightDist = GoToRight(String, Char, i);
+                if (LeftDist == -1) Result[i] = RightDist;
+                else if (RightDist == -1) Result[i] = LeftDist;
+                else
+                {
+                    int MinDist = std::min(LeftDist, RightDist);
+                    Result[i] = MinDist;
+                }
+            }
+        }
+        return Result;
+    }
+
+    int GoToRight(std::string& String, char Char, int Start)
+    {
+        int i = Start;
+        int RightBorder = String.size() - 1;
+        while (String[i] != Char)
+        {
+            i++;
+            if (i == RightBorder && String[i] != Char) return -1;
+        }
+        return abs(i - Start);
+    }
+    int GoToLeft(std::string& String, char Char, int Start)
+    {
+        int i = Start;
+        int LeftBorder = 0;
+        while (String[i] != Char)
+        {
+            i--;
+            if (i <= LeftBorder && String[i] != Char) return -1;
+        }
+        return abs(i - Start);
+    }
+};
+
+class Solution1
 {
 public:
     std::vector<int> shortestToChar(std::string String, char Char)
@@ -36,7 +86,8 @@ public:
 int main()
 {
     std::string Test1 = "loveleetcode";
+    std::string Test2 = "baaa";
     Solution daps;
-    daps.shortestToChar(Test1, 'e');    
+    daps.shortestToChar(Test2, 'b');    
     return 0;
 }
