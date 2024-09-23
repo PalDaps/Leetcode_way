f2b1240d2180d5596c5258f568100691000e0e8c
Daps <dashko.pavel@bk.ru>
2024-09-23 23:12:23 +0300

    I want to go Yandex

--- Leetcode_334_TBS/Source.cpp
+++ Leetcode_334_TBS/Source.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <vector>
 
+/*
+Идея: Есть три числа left, middle, right. Если right > middle > left по возрастанию, то можно
+ввести left = INT_MAX и обновлять его как только элемент массива <= left. Если это не выполняется
+запускать проверку на middle элемент и обновлять его.
+*/
+
 // The first
 // 6 : 02 PM
 // 7 / 22 / 2023
@@ -41,7 +47,10 @@
 // 23 : 37 PM
 // 27 / 08 / 2023 / SUCCESS
 
+// 20 : 47 PM
+// 23 / 09 / 2024 / The failure
 
+/*
 class Solution {
 public:
 	bool increasingTripltet(std::vector<int> nums) {
@@ -55,7 +64,28 @@ public:
 		return false;
 	}
 };
+*/
 
+class Solution
+{
+public:
+    bool increasingTriplet(std::vector<int>& Nums)
+    {
+        int Left = INT_MAX;
+        int Middle = INT_MAX;
+        int Right = INT_MAX;
+        for (size_t i = 0; i < Nums.size(); i++)
+        {
+            if (Left >= Nums[i]) Left = Nums[i];
+            else if (Middle >= Nums[i]) Middle = Nums[i];
+            else return true;
+        }
+        return false;
+    }
+};
 int main() {
+    Solution daps;
+    std::vector<int> Nums = { 1, 1, 1, 1, 1 };
+    daps.increasingTriplet(Nums);
 	return 0;
 }
