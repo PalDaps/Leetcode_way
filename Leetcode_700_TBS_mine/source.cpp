2a7dcdc6990d58352ab0d9bb4723f3e34fc84a2c
PalDaps <dashko.pavel@bk.ru>
2025-07-05 23:14:07 +0300

    easy first binary tree problem solved

---
+++ Leetcode_700_TBS_mine/source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+/*
+
+05.07.2025
+23:11
+
+Задача: Дано BST бинарное дерево. Нужно вернуть ноду, в которой значение будет равно входному параметру val.
+
+Идея: Просто в тупую через рекурсию.
+
+P.S. Очень быстро написал решение, но сам не до конца понимаю как оно работает. Плаваю в рекурсии. Не чувствую
+глубину рекурсии.
+
+*/
+
+struct TreeNode 
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode* searchBST(TreeNode* root, int val)
+    {
+        if (!root) return nullptr;
+        if (root->val == val) return root;
+        if (val > root->val) return searchBST(root->right, val);
+        else return searchBST(root->left, val);
+        return nullptr;
+    }
+};
