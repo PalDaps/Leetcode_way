d027bded140b183450739977c779c88d9a3c0568
PalDaps <dashko.pavel@bk.ru>
2025-07-21 19:38:35 +0300

    some tilted

--- Leetcode_430/source.cpp
+++ Leetcode_430/source.cpp
@@ -12,4 +12,29 @@
 1. Пройтись по списку, ищя child. 
 2. Вставить дочерний список после текущей ноды. 
 3. 
+
+P.S. 
+19:37
+21.07.2025
+Я не понимаю. И я в тильте!
+
 */
+
+class Node 
+{
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+
+Node* flatten(Node* head, Node* rest = nullptr) 
+{
+    if (!head) return rest;
+    Node* pTemp = flatten(head->next, rest);
+    head->next = flatten(head->child, pTemp);
+    if (head->next) head->next->prev = head;
+    head->child = nullptr;
+    return head;
+}
