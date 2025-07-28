4e3b7b0d1f35cd6e525766181caf356968a37334
PalDaps <dashko.pavel@bk.ru>
2025-07-28 23:21:49 +0300

    Hello acceptance? :)

---
+++ Leetcode_2235_TBS_mine/source.cpp
@@ -0,0 +1,20 @@
+/*
+
+28.07.2025
+23:20
+
+Задача: Сложить два int.
+
+Идея: :)
+
+P.S. 23:20 Почему такой низкий Acceptence? У задач, которые в разы сложнее Acceptance выше... Что-то тут нечисто :)
+
+*/
+
+class Solution {
+public:
+    int sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+};
