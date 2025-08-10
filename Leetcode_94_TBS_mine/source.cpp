cee49d68d3a853c5fab71ceee9c61ef1163971cf
PalDaps <dashko.pavel@bk.ru>
2025-08-11 01:27:28 +0300

    One more recursion

---
+++ Leetcode_94_TBS_mine/source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+/*
+
+11.08.2025
+01:24
+
+Задача: Дано бинарное дерево. Нужно вернуть все его корни в форме - the inorder traversal of its nodes' values.
+
+Идея: Просто пройтись по дерево и после прохода до конца левого поддерева добавлять значение ноды в массив.
+
+P.S. 1:26 Не совсем было понятно, что такое: the inorder traversal of its nodes' values. Я бы сказал, что это такое название кода ниже.
+
+*/
+
+class Solution {
+public:
+    void Solve(std::vector<int>& Result, TreeNode* root)
+    {
+        if (!root) return;
+        Solve(Result, root->left);
+        Result.push_back(root->val);
+        Solve(Result, root->right);
+    }
+    std::vector<int> inorderTraversal(TreeNode* root)
+    {
+        std::vector<int> Result;
+        Solve(Result, root);
+        return Result;
+    }
+};
